c5e3ac63db3253b3783d7156485b6b5ef203652a
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-10-28 11:33:08 -0700

    adding file to .gitignore

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f08a+h-f!mja0b&tgwpi^v1vshnlwmjzkru)o1*y_r=(-b!vk#'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -119,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+   BASE_DIR / 'static'
+]
 
 #adding uploadable media
 MEDIA_URL = '/media/'
