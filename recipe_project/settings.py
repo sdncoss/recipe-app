eddc6863b7b08846ec85778b7e355ee940822034
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-10-31 14:18:31 -0700

    changed databases configuration

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -141,5 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(
+    dj_database_url.config(
+        conn_max_age=500,
+        ssl_require=True
+    )
+)
